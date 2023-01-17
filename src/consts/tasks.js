cc7ea01c109dd2d12a4c00d1a250ee143ea66cfa
roginaldosemog <roginaldosemog@gmail.com>
2023-01-16 23:23:07 -0300

    implementa interval scheduling
    Co-authored-by: Daniel Primo <danielprimo9@gmail.com>

---
+++ src/consts/tasks.js
@@ -0,0 +1,83 @@
+export const DEFAULT_TASKS = [
+  {
+    nome: 'Assistir a série Wandinha',
+    horarioInicial: 1,
+    horarioFinal: 2
+  },
+  {
+    nome: 'Jogar bocha',
+    horarioInicial: 3,
+    horarioFinal: 5
+  },
+  {
+    nome: 'Tentar um novo visual no cabeleireiro',
+    horarioInicial: 0,
+    horarioFinal: 1
+  },
+  {
+    nome: 'Iniciar nas aulas de forró',
+    horarioInicial: 7,
+    horarioFinal: 8
+  },
+  {
+    nome: 'Aprender um novo idioma',
+    horarioInicial: 7,
+    horarioFinal: 12
+  },
+  {
+    nome: 'Fazer aulas de mergulho',
+    horarioInicial: 7,
+    horarioFinal: 12
+  },
+  {
+    nome: 'Terminar de assistir a série Lost',
+    horarioInicial: 7,
+    horarioFinal: 12
+  },
+  {
+    nome: 'Stalkear o Endrick',
+    horarioInicial: 7,
+    horarioFinal: 12
+  },
+  {
+    nome: 'Aprender a mexer com social media',
+    horarioInicial: 7,
+    horarioFinal: 12
+  },
+  {
+    nome: 'Dar comida para os pombos na praça',
+    horarioInicial: 7,
+    horarioFinal: 12
+  },
+  {
+    nome: 'Divulgar curso na internet',
+    horarioInicial: 7,
+    horarioFinal: 12
+  },
+  {
+    nome: 'Fazer um vídeo falando como foi sua trajetória',
+    horarioInicial: 7,
+    horarioFinal: 12
+  },
+  {
+    nome: 'Aprender a jogar beach tennis',
+    horarioInicial: 7,
+    horarioFinal: 12
+  },
+  {
+    nome: 'Pescar',
+    horarioInicial: 7,
+    horarioFinal: 12
+  },
+  {
+    nome: 'Beber Whisky',
+    horarioInicial: 7,
+    horarioFinal: 12
+  },
+  {
+    nome: 'Ouvir o novo CD do Tim Maia',
+    horarioInicial: 7,
+    horarioFinal: 12
+  },
+
+]
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,74 @@
-function HomePage() {
+import { useState, useEffect } from 'react';
+import TaskCard from '../components/TaskCard';
+import { DEFAULT_TASKS } from '../consts/tasks';
+import { getTiteSchedule } from '../utils/greedy';
+
+const HomePage = () => {
+  const [tasks, setTasks] = useState(null);
+  const [isResultMode, setIsResultMode] = useState(false);
+  const [isCreateMode, setIsCreateMode] = useState(false);
+
+  const loadTasks = () => {
+    const loadedTasks = localStorage.getItem('tasks');
+    if (!!loadedTasks) {
+      setTasks(JSON.parse(loadedTasks));
+    } else {
+      setTasks(DEFAULT_TASKS);
+    }
+  }
+
+  const removeTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  }
+
+  const handleCreateModeBtn = () => setIsCreateMode(!isCreateMode);
+  const handleResultModeBtn = () => {
+    getTiteSchedule(tasks)
+    setIsResultMode(!isResultMode)
+  };
+  const handleResetLocalStorageBtn = () => localStorage.clear();
+
+  const renderTaskCards = tasks?.map((taskData, index) => <TaskCard key={`task-${index}`} index={index} taskData={taskData} removeTask={removeTask} />);
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  useEffect(() => {
+    !!tasks && localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
   return (
-    <div className="App">
-      <h1>Tite Simulator</h1>
+    <div className="home">
+      <div className='container'>
+        <section className="text-center">
+          <h1>Tite Simulator</h1>
+          <p>Depois de vergonhosamente perder a copa do mundo para a Croácia, Tite precisa da sua ajuda pra planejar as férias dele.</p>
+          <p>Adicione ou remova tarefas e clique no botão "Planejar Férias" para ver a nova agenda do professor.</p>
+        </section>
+        {
+          isResultMode ?
+            (<>
+              <section className='button-list'>
+                <button type="button" className="btn btn-primary" onClick={handleResultModeBtn}>Retornar às tarefas</button>
+              </section >
+            </>)
+            :
+            (<>
+              <section className='button-list'>
+                <button type="button" className="btn btn-primary" onClick={handleResetLocalStorageBtn}>Reiniciar tarefas</button>
+                <button type="button" className="btn btn-primary" onClick={handleCreateModeBtn}>Adicionar Tarefa</button>
+                <button type="button" className="btn btn-success" onClick={handleResultModeBtn}>Planejar Férias</button>
+              </section >
+              <section>
+                <div className="row row-cols-3">
+                  {renderTaskCards}
+                </div>
+              </section>
+            </>)
+        }
+      </div>
     </div>
   );
 }
